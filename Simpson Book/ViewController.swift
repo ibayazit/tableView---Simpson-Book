8ead82cc4bb5e70cc85d45999230f1dcb96da2d7
bayazit <bayazit@bayazit-macbook-air.local>
2019-02-17 14:55:11 +0300

    Initial Commit

---
+++ Simpson Book/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simpson Book
+//
+//  Created by bayazit on 17.02.2019.
+//  Copyright © 2019 ibrahim bayazit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
